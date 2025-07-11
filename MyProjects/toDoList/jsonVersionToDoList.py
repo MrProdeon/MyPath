f1111495a142beb902c4fe8f268560f0f0711fb3
Prodeon <prodeon21gmail.com>
2025-07-11 17:56:13 +0300

    added a funcs and ask the user what he want

--- MyProjects/toDoList/jsonVersionToDoList.py
+++ MyProjects/toDoList/jsonVersionToDoList.py
@@ -34,7 +34,7 @@ def print_tasks(task_list: list) -> None:
 
 def end_or_continue() -> bool:
     """Уточнение у пользователя нужно ли добавить ещё задачи"""
-    answer = input('Хотите ещё добавить задачу ? д если да, н если нет ---> ')
+    answer = input('Хотите посмотреть текущие задачи? д если да, н если нет ---> ')
     while answer not in ('д', 'н'):
         answer = input('Неверный формат ввода. д если да, н если нет ---> ')
     return answer == 'д'
@@ -63,7 +63,26 @@ def load_tasks(filename='tasks.json'):
     except json.JSONDecodeError:
         return []
 
+def add_more_task(more=True):
+    return more
 
+
+# noinspection PyUnreachableCode
+def want_mark():
+    while True:
+        mark = input('Хотите отметить какую-то задачу как выполненную? д если да, н если нет ---> ')
+        while mark not in ('д', 'н'):
+            mark = input('Хотите отметить какую-то задачу как выполненную? д если да, н если нет ---> ')
+        if mark == 'д':
+            try:
+                task_id = int(input('Введите id вашей задачи ---> '))
+                mark_done(toDoList, task_id)
+                print_tasks(toDoList)
+                continue
+            except ValueError as error:
+                print(f'Ошибка {error} - id должен быть числом')
+        else:
+            break
 
 
 def main() -> None:
@@ -83,18 +102,51 @@ def main() -> None:
         max_id = 0
 
     while True:
-        case : str = input('Введите ваше дело : ')
-        time : str = input('До какого времени выполнить (Синтаксис = 01.01.2001 ) : ')
-        tag : str = input('Введите пометку, если требуется : ')
-        add_a_case(get_case(case, time, tag))
+        what_to_do = input('Что вы хотите сделать? д - добавить дело п - просмотреть дела о - отметить выполненным ---> ').lower()
+        if what_to_do == 'д':
+            while add_more_task():
+                case : str = input('Введите ваше дело : ')
+                time : str = input('До какого времени выполнить (Синтаксис = 01.01.2001 ) : ')
+                tag : str = input('Введите пометку, если требуется : ')
+                add_a_case(get_case(case, time, tag))
+                more_task = input('Хотите добавить ещё одну задачу? д - если да, н - если нет ---> ')
+                if more_task == 'н':
+                    break
+        if what_to_do == 'п':
+            task = get_tasks_by_progress(toDoList, progress=None)
+            print_tasks(task)
+            want_mark()
+            print('Обновлённый список задач: ')
+            print_tasks(task)
+            save_tasks(toDoList)
+            break
+        if what_to_do == 'о':
+            task = get_tasks_by_progress(toDoList, progress=None)
+            print_tasks(task)
+            want_mark()
+            print('Обновлённый список задач: ')
+            print_tasks(task)
+            save_tasks(toDoList)
+            break
 
-        if not end_or_continue():
+
+        if  end_or_continue():
 
             what_progress = input('Вывести все задачи, только завершенные или только невыполненные в - все, н - невыполненные, з - завершенные ---> ')
             if what_progress == 'в':
                 tasks = get_tasks_by_progress(toDoList)
+                print_tasks(tasks)
+                want_mark()
+                print('Обновлённый список задач: ')
+                print_tasks(tasks)
+                save_tasks(toDoList)
             elif what_progress == 'н':
                 tasks = get_tasks_by_progress(toDoList, progress='Не выполнено')
+                print_tasks(tasks)
+                want_mark()
+                print('Обновлённый список задач: ')
+                print_tasks(tasks)
+                save_tasks(toDoList)
             elif what_progress == 'з':
                 tasks = get_tasks_by_progress(toDoList, progress='Выполнено')
             else:
@@ -103,20 +155,21 @@ def main() -> None:
 
             print_tasks(tasks)
 
-            mark = input('Хотите отметить какую-то задачу как выполненную? д если да, н если нет ---> ')
-            while mark not in ('д', 'н'):
-                mark = input('Хотите отметить какую-то задачу как выполненную? д если да, н если нет ---> ')
-            if mark == 'д':
-                try:
-                    task_id = int(input('Введите id вашей задачи ---> '))
-                    mark_done(toDoList, task_id)
-                except ValueError as error:
-                    print(f'Ошибка {error} - id должен быть числом')
+            # mark = input('Хотите отметить какую-то задачу как выполненную? д если да, н если нет ---> ')
+            # while mark not in ('д', 'н'):
+            #     mark = input('Хотите отметить какую-то задачу как выполненную? д если да, н если нет ---> ')
+            # if mark == 'д':
+            #     try:
+            #         task_id = int(input('Введите id вашей задачи ---> '))
+            #         mark_done(toDoList, task_id)
+            #     except ValueError as error:
+            #         print(f'Ошибка {error} - id должен быть числом')
 
-            print('Обновлённый список задач: ')
-            print_tasks(tasks)
-            save_tasks(toDoList)
+            # print('Обновлённый список задач: ')
+            # print_tasks(tasks)
+            # save_tasks(toDoList)
             break
+        break
 
 
 main()
